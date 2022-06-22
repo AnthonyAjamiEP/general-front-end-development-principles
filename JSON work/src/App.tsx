c12a33e867d25ce94a39d66a38e19a26ad3b7c91
AnthonyAjami <anthony.ajami@epita.fr>
2022-06-22 15:22:05 +0200

    register users

--- JSON work/src/App.tsx
+++ JSON work/src/App.tsx
@@ -1,20 +1,22 @@
 import React from 'react'
+import { Routes, Route, Link } from "react-router-dom"
 
-import Home from './pages/Home'
+import Home from "./pages/Home"
 import Post from './pages/Post'
-
-import { Route, Routes, Link } from 'react-router-dom'
+import RegisterPage from './pages/RegisterPage'
 
 const App = () => {
   return (<>
     <div id="nav">
       <Link to='/'>Home</Link>
       <Link to='/post'>Posts</Link>
+      <Link to="/register">Register</Link>
     </div>
     <div className='container'>
       <Routes>
-        <Route path='/' element={<Home />} />
-        <Route path='/post' element={<Post />} />
+        <Route path="/" element={<Home />} />
+        <Route path="/post" element={<Post />} />
+        <Route path="/register" element={<RegisterPage />} />
       </Routes>
     </div>
   </>)
--- JSON work/src/pages/RegisterPage.tsx
+++ JSON work/src/pages/RegisterPage.tsx
@@ -1,7 +1,66 @@
-return (
-    <form onSubmit={onSubmitHandler}>
-        <div className="form-row">
-            <label>Username</label>
-        </div>
-    </form>
-)
+import { useState, ChangeEvent, FormEvent } from 'react'
+import { useNavigate } from "react-router-dom"
+
+import { User } from '../types/user'
+import { register } from '../services/auth'
+
+const RegisterPage = () => {
+    let navigate = useNavigate()
+    const [form, setForm] = useState<User>({
+        username: '',
+        email: '',
+        password: ''
+    })
+
+    const onChangeHandler = (event: ChangeEvent<HTMLInputElement>) => {
+        const { name, value }: { name: string, value: string } = event.target
+        setForm({ ...form, [name]: value })
+    }
+
+    const onSubmitHandler = async (event: FormEvent<HTMLFormElement>) => {
+        event.preventDefault()
+        if (await register(form)) {
+            navigate('/')
+        }
+    }
+
+    return (
+        <form onSubmit={onSubmitHandler}>
+            <div className="form-row">
+                <label>Username</label>
+                <input
+                    type="text"
+                    name="username"
+                    value={form.username}
+                    onChange={onChangeHandler}
+                />
+            </div>
+
+            <div className="form-row">
+                <label>Email</label>
+                <input
+                    type="email"
+                    name="email"
+                    value={form.email}
+                    onChange={onChangeHandler}
+                />
+            </div>
+
+            <div className="form-row">
+                <label>Password</label>
+                <input
+                    type="password"
+                    name="password"
+                    value={form.password}
+                    onChange={onChangeHandler}
+                />
+            </div>
+
+            <div className="form-row">
+                <button type='submit'>Send</button>
+            </div>
+        </form>
+    )
+}
+
+export default RegisterPage
